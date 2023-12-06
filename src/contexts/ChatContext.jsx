3672d54902b7f7b9619bb58266f7813b471ea396
Youssef Ab <youssef.abelouass@gmail.com>
2023-12-06 23:26:55 +0100

    fixing a bug where the avatar doesn't appear in the chatroom

--- src/contexts/ChatContext.jsx
+++ src/contexts/ChatContext.jsx
@@ -1,6 +1,17 @@
 import React, { useContext, useReducer } from "react"
 import { useAuth } from "./AuthContext.jsx"
 
+import avatar1 from "../assets/avatars/Avatar-1.png"
+import avatar2 from "../assets/avatars/Avatar-2.png"
+import avatar3 from "../assets/avatars/Avatar-3.png"
+import avatar4 from "../assets/avatars/Avatar-4.png"
+import avatar5 from "../assets/avatars/Avatar-5.png"
+import avatar6 from "../assets/avatars/Avatar-6.png"
+import avatar7 from "../assets/avatars/Avatar-7.png"
+import avatar8 from "../assets/avatars/Avatar-8.png"
+import avatar9 from "../assets/avatars/Avatar-9.png"
+import avatar10 from "../assets/avatars/Avatar-10.png"
+
 const ChatContext = React.createContext()
 
 export function useChat() {
